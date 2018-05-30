f94cfa786f07ddf94e0241ceffe5baf116b1558b
Frikk H Fossdal <frikkfossdal@gmail.com>
2018-05-30 12:58:08 +0200

    added class. Kevin needs a charger

---
+++ src/vec2key.cpp
@@ -0,0 +1,8 @@
+//
+//  vec2key.cpp
+//  fluffy-octo-potato
+//
+//  Created by Frikk Fossdal on 30.05.2018.
+//
+
+#include "vec2key.hpp"
---
+++ src/vec2key.hpp
@@ -0,0 +1,13 @@
+//
+//  vec2key.hpp
+//  fluffy-octo-potato
+//
+//  Created by Frikk Fossdal on 30.05.2018.
+//
+
+#ifndef vec2key_hpp
+#define vec2key_hpp
+
+#include <stdio.h>
+
+#endif /* vec2key_hpp */
